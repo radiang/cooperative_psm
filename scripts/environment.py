078a9bc51eb01c3d67549288510da08b01980081
Radian Azhar <radiang91@gmail.com>
2018-01-29 10:22:41 -0500

    Added gazebo stretched object environment, end force plot, constrained joint roll, and tuned PID

--- scripts/environment.py
+++ scripts/environment.py
@@ -20,7 +20,7 @@ class environment(object):
         self.init_object = np.ones((self.num,3))
         self.new_object = np.ones((self.num,3))
         self.change = np.ones((self.num,3))
-        self.force = np.ones((4self.num,3,3))
+        self.force = np.ones((self.num,3,3))
         self.k=k
         self.namedict = {'one':'PSM1', 'two':'PSM2', 'three':'PSM3', 'four':'PSM4'}
         self.rot_tool = [None]*4
@@ -29,9 +29,10 @@ class environment(object):
 
     def initial_object(self):
         for i in range(self.num):
-            (self.init_pose[i], self.rot_tool) = self.gazebo_service_call(self.namedict[self.name[i]]+'::tool_wrist_link','')
-            
+            (self.init_pose[i], self.rot_tool) = self.gazebo_service_call(self.namedict[self.name[i]]+'::tool_wrist_link','world')
+        for i in range(self.num):    
             self.init_object[i]=self.cyclic_substraction(self.init_pose,self.init_object,i,self.num-1)
+            print('init',self.init_pose[i])
 
     def gazebo_service_call(self,x,y):
         rospy.wait_for_service('/gazebo/get_link_state')
@@ -53,7 +54,7 @@ class environment(object):
         except rospy.ServiceException, e:
             print "Service call failed: %s"%e
 
-    def gazebo_force_call(self,force):
+    def gazebo_force_call(self,force,i):
         get = rospy.ServiceProxy('/gazebo/apply_body_wrench',ApplyBodyWrench)
         wrench = gm.Wrench() 
         wrench.force.x = force[0]
@@ -64,38 +65,56 @@ class environment(object):
         wrench.torque.z = 0 
         body_name = self.namedict[self.name[i]]+'::tool_wrist_link'
 
+        #print(wrench)
         try: 
-            get(body_name=body_name, wrench=wrench,duration = rospy.Duration(.5))
+            get(body_name=body_name,reference_frame='world',wrench=wrench,duration = rospy.Duration(.4))
 
         except rospy.ServiceException as e:
             print e
             
     def cyclic_substraction(self,x,y,num,num_max):
-        if (num!=num_max):
-            y[num]=x[num]-x[num-1]
+        if (num==0):
+            y[num]=x[num_max]-x[num]
         else: 
-            y[num]=x[num]-x[num_max]
-
+            y[num]=x[num-1]-x[num]
         return y[num]
 
     def cyclic_numbering(self,x,num_max):
         if (x>num_max):
             x = 0
         else:
+            x = x
         return x 
-
     def calculate_new_object(self): 
         for i in range(self.num):
-            (self.new_pose[i], self.rot_tool) = self.gazebo_service_call(self.namedict[self.name[i]]+'::tool_wrist_link','')
+            (self.new_pose[i], self.rot_tool) = self.gazebo_service_call(self.namedict[self.name[i]]+'::tool_wrist_link','world')
             self.new_object[i]=self.cyclic_substraction(self.new_pose,self.new_object,i,self.num-1)
+    
 
     def calculate_force(self): 
         for i in range(self.num):
             #Should use neighbor algorithm, right now neighbors are sequential i's
-            j=cyclic_numbering(i+1,self.num)
-            self.force[i][0]=self.k*(self.new_object[i]-self.init_object[i])*np.divide(self.new_object[i],np.linag.norm(self.new_object[i]))
-            self.force[i][1]=-self.k*(self.new_object[j]-self.init_object[j])*np.divide(self.new_object[j],np.linag.norm(self.new_object[j]))
-            self.force[i][2]=self.force[i][0]+self.force[i][1]
+            if(self.num>2):
+                j=self.cyclic_numbering(i+1,self.num-1)
+                self.force[i][0]=self.k*(self.new_object[i]-self.init_object[i])*np.divide(self.new_object[i],np.linalg.norm(self.new_object[i]))
+                self.force[i][1]=-self.k*(self.new_object[j]-self.init_object[j])*np.divide(self.new_object[j],np.linalg.norm(self.new_object[j]))
+                self.force[i][2]=self.force[i][0]+self.force[i][1]
+            else: 
+                self.force[i][0]=self.k*(np.linalg.norm(self.new_object[i])-np.linalg.norm(self.init_object[i]))*np.divide(self.new_object[i],np.linalg.norm(self.new_object[i]))
+            #print(i, self.force[i][0])
+    def message_making(self,force,i):
+        msg = gm.WrenchStamped()
+
+        msg.wrench.force.x = force[0]
+        msg.wrench.force.y = force[1]
+        msg.wrench.force.z = force[2]
+        msg.wrench.torque.x = np.linalg.norm(np.array(force))
+        msg.wrench.torque.y = 0
+        msg.wrench.torque.z = 0 
+
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id= str(i)
+        return msg
 
 if __name__ == '__main__':
     rospy.init_node("object_node",anonymous=True)
@@ -105,16 +124,41 @@ if __name__ == '__main__':
     num = rospy.get_param('/number')
     gazebo_on = rospy.get_param('/gazebo_on')
     namedict = {'one':'PSM1', 'two':'PSM2', 'three':'PSM3', 'four':'PSM4'}
+
+    p = [None]*num
+    for i in range(num):
+        p[i]=rospy.Publisher('/psm_sense/'+namedict[name[i]]+'/tool_forces',gm.WrenchStamped, queue_size=10)
+
     
-    k=10
+    k=100 #stiffness of object
 
     env = environment(name,num,k)    
     r = rospy.Rate(1600)
-
+    rospy.sleep(1)
+    j = 0
     while not rospy.is_shutdown():          
+        
         env.calculate_new_object()
         env.calculate_force()
+        if (j == 0):
+            print('new_pose',env.new_pose)
+            print('force',env.force)
+            rospy.sleep(1)
+        print('init_object',env.init_object)
+        print('new_object',env.new_object)
+        print('force',env.force)
+
+
         for i in range(num):
-            env.gazebo_force_call(env.force[i][3])
+            if(num>2):
+                env.gazebo_force_call(env.force[i][2],i)
+                msg = env.message_making(env.force[i][2],i)
+                p[i].publish(msg)
+            else:
+                env.gazebo_force_call(env.force[i][0],i)
+                msg = env.message_making(env.force[i][0],i)
+                p[i].publish(msg)
+
+        j = j + 1
         r.sleep()
         
--- scripts/gazebo_joint_controller.py
+++ scripts/gazebo_joint_controller.py
@@ -22,15 +22,17 @@ class gazebo_wrapper():
         print('yes1')
 
         self.j=[]
-        self.j.append([None]*3)
-        self.j.append([None]*3)
-        self.j.append([None]*3)
+        self.j.append([None]*4)
+        self.j.append([None]*4)
+        self.j.append([None]*4)
+
 
         for i in range(self.num):
             rospy.Subscriber('/' + self.name[i], JointState, self.callback)
             self.j[i][0]=rospy.Publisher('/dvrk_psm/'+self.namedict[self.name[i]]+'/outer_yaw_joint/SetPositionTarget',Float64, queue_size=10)
             self.j[i][1]=rospy.Publisher('/dvrk_psm/'+self.namedict[self.name[i]]+'/outer_pitch_joint_1/SetPositionTarget',Float64, queue_size=10)
             self.j[i][2]=rospy.Publisher('/dvrk_psm/'+self.namedict[self.name[i]]+'/outer_insertion_joint/SetPositionTarget',Float64, queue_size=10)
+            self.j[i][3]=rospy.Publisher('/dvrk_psm/'+self.namedict[self.name[i]]+'/outer_roll_joint/SetPositionTarget',Float64, queue_size=10)
             
         print('yes2')
         
@@ -42,16 +44,21 @@ class gazebo_wrapper():
             for i in range(self.num):
                 self.j[i][0].publish(0.0)
                 self.j[i][1].publish(0.0)
-                self.j[i][2].publish(0.1)    
+                self.j[i][2].publish(0.15)
+                self.j[i][3].publish(0.0)    
                 print("yes4")
             rospy.sleep(0.1)
-
     def callback(self,data):
-        self.j[self.numdict[data.header.frame_id]][int(data.velocity[0])].publish(data.position[0])
-        
+        if(int(data.velocity[0])!=2):
+            self.j[self.numdict[data.header.frame_id]][int(data.velocity[0])].publish(data.position[0])
+        #else:
+         #   self.j[self.numdict[data.header.frame_id]][int(data.velocity[0])].publish(0.15)
+        #self.j[0][3].publish(0.0)
+        #self.j[1][3].publish(0.0)
+
 if __name__ == '__main__':
     a = gazebo_wrapper()
-    rospy.sleep(2)
+    rospy.sleep(0.5)
     a.pupu()
     rospy.spin()
    
