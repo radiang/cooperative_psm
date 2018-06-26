bb5f0497e57a88ffe5b30a39a35072e2b3382688
Radian <radiang91@gmail.com>
2018-06-25 20:09:41 -0400

    stupid

--- scripts/calibrate_psms.py
+++ scripts/calibrate_psms.py
@@ -5,7 +5,7 @@
 # gives transformation_matrix.txt and transformation_matrix.npz files as an output
 
 from psm_coop import tracker
-from scipy.cluster.vq import kmeans, whiten
+#from scipy.cluster.vq import kmeans, whiten
 import numpy as np
 import dvrk
 import rospy
@@ -14,22 +14,27 @@ if __name__ == '__main__':
 
     p = [None]*2
 
-    p1 = dvrk.psm('PSM1')
-    p2 = dvrk.psm('PSM2')
+    p1 = dvrk.psm('PSM2')
+    p2 = dvrk.psm('PSM1')
 
-    p1.move_joint_some(np.array([-1, -0.5, 0.1]), np.array([0, 1, 2]))
-    p2.move_joint_some(np.array([-1, -0.5, 0.1]), np.array([0, 1, 2]))
+    limit_q1 = 0.75
+    limit_q2 = 0.25
+    limit_q3 = 0.05
+
+    p1.move_joint_some(np.array([-limit_q1, -limit_q2, limit_q3]), np.array([0, 1, 2]))
+    p2.move_joint_some(np.array([-limit_q1, -limit_q2, limit_q3]), np.array([0, 1, 2]))
 
     rate = 200
-    tf = 10
+    tf = 2
 
     r = rospy.Rate(rate)
 
-    z = rate *tf
-    z2 = 10
+    z = 10
+    z2 = rate *tf
 
-    q1 = np.linspace(-1, 1, num=z)
-    q2 = np.linspace(-.5, .5, num=z2)
+    
+    q1 = np.linspace(-limit_q1, limit_q1, num=z)
+    q2 = np.linspace(-limit_q2, limit_q2, num=z2)
     i = 0
     j = 0
 
@@ -39,14 +44,21 @@ if __name__ == '__main__':
 
     while i < z and not rospy.is_shutdown():
         j = 0
+        p1.move_joint_some(np.array([q1[i], q2[j], limit_q3]), np.array([0, 1, 2]))
+        p2.move_joint_some(np.array([q1[i], q2[j], limit_q3]), np.array([0, 1, 2]))
+            
+
         while j < z2 and not rospy.is_shutdown():
-            p1.move_joint_some(np.array([q1[i], q2[j], 0.1]), np.array([0, 1, 2]), False)
-            p2.move_joint_some(np.array([q1[i], q2[j], 0.1]), np.array([0, 1, 2]), False)
+            p1.move_joint_some(np.array([q1[i], q2[j], limit_q3]), np.array([0, 1, 2]), False)
+            p2.move_joint_some(np.array([q1[i], q2[j], limit_q3]), np.array([0, 1, 2]), False)
+            
             r.sleep()
             j = j + 1
 
-
         i = i + 1
 
+    
+    p2.move_joint_some(np.array([-0, -0, limit_q3]), np.array([0, 1, 2]))
+    p1.move_joint_some(np.array([-0, -0, limit_q3]), np.array([0, 1, 2]))
     x = 0
 
